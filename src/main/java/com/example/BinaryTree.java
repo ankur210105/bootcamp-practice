5cceb5863d4ae22a9acf3653563f81a3008252e4
ankur210105 <150512452+ankur210105@users.noreply.github.com>
2026-06-04 12:24:54 +0530

    basics of binary tree

--- src/main/java/com/example/BinaryTree.java
+++ src/main/java/com/example/BinaryTree.java
@@ -29,5 +29,8 @@ public class BinaryTree {
         tree.root.right=new Node(30);
         tree.root.left.left=new Node(40);
         tree.root.left.right=new Node(50);
+        tree.root.right.left= new Node(60);
+        tree.root.right.right= new Node(70);
+
     }
 }
